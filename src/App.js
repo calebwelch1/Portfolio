4ef35f8cb8bce17710d03dd536c645c381abff3f
calebwelch <caleb.welch11@gmail.com>
2020-08-14 17:01:09 -0400

    remaking portfolio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import IconBreadcrumbs from "./components/IconBreadCrumbs";
+
+export default function App() {
+  return (
+    <div className="App">
+      <IconBreadcrumbs />
+    </div>
+  );
+}
